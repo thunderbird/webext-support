63cb30c70ad7957586af185a3b406e1b2e2b79fe
John Bieling <john.bieling@gmx.de>
2020-07-24 15:06:13 +0200

    move api to root

--- wrapper-apis/BootstrapLoader/bootstrap.js
+++ wrapper-apis/BootstrapLoader/bootstrap.js
@@ -1,13 +1,29 @@
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
+console.log(extension);
+console.log(addon.id);
+console.log(addon.version);
+console.log(messenger);
+
+
 function startup(data, reason) {
   console.log("startup");  
-  console.log(data);
-  console.log(reason);
+  if (reason == APP_STARTUP) {
+      console.log("APP_STARTUP")
+  } else if (reason == ADDON_ENABLE) {
+      console.log("ADDON_ENABLE")
+  } else {
+    console.log(reason);
+  }  
 }
 
 function shutdown(data, reason) {
   console.log("shutdown");  
-  console.log(data);
-  console.log(reason);
+  if (reason == APP_SHUTDOWN) {
+      console.log("APP_SHUTDOWN")
+  } else if (reason == ADDON_DISABLE) {
+      console.log("ADDON_DISABLE")
+  } else {
+    console.log(reason);
+  }  
 }
