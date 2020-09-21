ac3625ce0ba6617defc69a7390e95a9cab67c014
John Bieling <john.bieling@gmx.de>
2020-09-21 08:38:42 +0200

    Update implementation.js

--- auxiliary-apis/LegacyPrefs/implementation.js
+++ auxiliary-apis/LegacyPrefs/implementation.js
@@ -1,3 +1,16 @@
+/*
+ * This file is provided by the addon-developer-support repository at
+ * https://github.com/thundernest/addon-developer-support
+ *
+ * Version: 1.1
+ *
+ * Author: John Bieling (john@thunderbird.net)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
   
