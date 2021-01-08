3beac6e37d2b3af01b98b9ffe12da3a7ec82dfd7
John Bieling <john.bieling@gmx.de>
2021-01-08 15:03:35 +0100

    add back link to the addon-developer-support repo

--- auxiliary-apis/LegacyMenu/implementation.js
+++ auxiliary-apis/LegacyMenu/implementation.js
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
  *
+ * This file is provided by the addon-developer-support repository at
+ * https://github.com/thundernest/addon-developer-support
+ * 
  * Do not remove or change this comment.
  *
  * The initial author of the code is:
