bac8c885f0470c45dda59347ef852639fa00ee43
John Bieling <john.bieling@gmx.de>
2022-04-06 11:58:52 +0200

    fix context overwrites

--- auxiliary-apis/LegacyCSS/implementation.js
+++ auxiliary-apis/LegacyCSS/implementation.js
@@ -2,7 +2,13 @@
  * This file is provided by the addon-developer-support repository at
  * https://github.com/thundernest/addon-developer-support
  *
-  * Author: John Bieling (john@thunderbird.net)
+ * Version 1.2
+ * - fix multiple context not overwriting class members
+ * 
+ * Version: 1.1
+ * - initial release
+ *
+ * Author: John Bieling (john@thunderbird.net)
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -16,97 +22,155 @@ var { ExtensionCommon } = ChromeUtils.import(
 var { ExtensionSupport } = ChromeUtils.import(
   "resource:///modules/ExtensionSupport.jsm"
 );
+var { ExtensionUtils } = ChromeUtils.import(
+  "resource://gre/modules/ExtensionUtils.jsm"
+);
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var { ExtensionError } = ExtensionUtils;
 
 var LegacyCSS = class extends ExtensionCommon.ExtensionAPI {
+  // Alternative to defining a constructor to init the class, is to use the
+  // onStartup event. However, this causes the API to be instantiated after the
+  // add-on has been loaded, not when the API is first used.
+  constructor(...args) {
+    // The only parameter is extension, but it could change in the future.
+    // super() will add the extension as a member of this.
+    super(...args);
+    let extensionApi = this;
 
-  // These functions are "private" helper function and cannot be reached from outside.
-  trackWindow(window) {
-    if (!this.windowTracker.includes(window))
-      this.windowTracker.push(window);
-  }
+    this.windowTracker = new Map();
+    this.windowOpenListener = new Set();
+    this.uniqueRandomID = this.extension.uuid + "_" + this.extension.instanceId;
 
-  untrackWindow(window) {
-    this.windowTracker = this.windowTracker.filter(w => w != window);
-  }
-
-  getWindows(url) {
-    return this.windowTracker.filter(w => w.location.href == url);
-  }
-
-
-
-  // The API implementation.
-  getAPI(context) {
-    context.callOnClose(this);
-
-    this.context = context;
-    this.windowTracker = [];
-    this.windowOpenListener = {};
-    this.windowOpenListenerNext = 0;
-    this.uniqueRandomID = context.extension.uuid + "_" + context.extension.instanceId;
-
-    let self = this;
+    this.chromeData = null;
+    this.resourceData = null;
 
     ExtensionSupport.registerWindowListener(
       "windowListener_" + this.uniqueRandomID,
       {
-        async onLoadWindow(window) {
-          self.trackWindow(window);
-          for (let listener of Object.values(self.windowOpenListener)) {
+        onLoadWindow(window) {
+          for (let listener of extensionApi.windowOpenListener.values()) {
             listener(window.location.href);
           }
         },
         onUnloadWindow(window) {
-          self.untrackWindow(window);
+          extensionApi.untrackAllCssFiles(window);
         }
       }
     );
+  }
 
+  // These functions are "private" helper function and cannot be reached from outside.
+  trackCssFile(window, cssFile) {
+    let cssFiles = this.windowTracker.get(window) || [];
+    cssFiles.push(cssFile)
+    this.windowTracker.set(window, cssFiles);
+  }
+
+  hasCssFile(window, cssFile) {
+    let cssFiles = this.windowTracker.get(window) || [];
+    return cssFiles.includes(cssFile);
+  }
+
+  untrackAllCssFiles(window) {
+    this.windowTracker.delete(window);
+  }
+
+  // The API implementation.
+  getAPI(context) {
+    let extensionApi = this;
     return {
       LegacyCSS: {
         onWindowOpened: new ExtensionCommon.EventManager({
           context,
           name: "LegacyCSS.onWindowOpened",
           register: (fire) => {
-            let listenerId = self.windowOpenListenerNext++;
-            self.windowOpenListener[listenerId] = fire.sync;
+            extensionApi.windowOpenListener.add(fire.sync);
             return () => {
-              delete self.windowOpenListener[listenerId];
+              extensionApi.windowOpenListener.delete(fire.sync);
             };
           },
         }).api(),
 
         async inject(url, cssFile) {
-          let windows = self.getWindows(url);
-          if (windows.length == 0) {
-            // We have not seen this window, scan all open windows.
-            for (let w of Services.wm.getEnumerator(null)) {
-              self.trackWindow(w);
+          let path = extensionApi.extension.rootURI.resolve(cssFile);
+          let injected = false;
+
+          for (let window of Services.wm.getEnumerator(null)) {
+            if (
+              window.location.href != url ||
+              extensionApi.hasCssFile(window, path)
+            ) {
+              continue;
             }
-            windows = self.getWindows(url);
-          }
 
-          // If the window is still not know, return false
-          if (windows.length == 0) {
-            return false;
-          }
-
-          let path = context.extension.rootURI.resolve(cssFile);
-          for (let window of windows) {
             let element = window.document.createElement("link");
-            element.setAttribute("css_injected", self.uniqueRandomID);
+            element.setAttribute("css_injected", extensionApi.uniqueRandomID);
             element.setAttribute("rel", "stylesheet");
             element.setAttribute("href", path);
             window.document.documentElement.appendChild(element);
+
+            extensionApi.trackCssFile(window, path);
+            injected = true;
           }
+          return injected;
+        },
+
+        registerChromeUrl(data) {
+          if (extensionApi.chromeData || extensionApi.resourceData) {
+            throw new ExtensionError(`Cannot call registerChromeUrl more than once.`);
+          }
+
+          const aomStartup = Cc[
+            "@mozilla.org/addons/addon-manager-startup;1"
+          ].getService(Ci.amIAddonManagerStartup);
+          const resProto = Cc[
+            "@mozilla.org/network/protocol;1?name=resource"
+          ].getService(Ci.nsISubstitutingProtocolHandler);
+
+          let chromeData = [];
+          let resourceData = [];
+          for (let entry of data) {
+            if (entry[0] == "resource") resourceData.push(entry);
+            else chromeData.push(entry)
+          }
+
+          if (chromeData.length > 0) {
+            const manifestURI = Services.io.newURI(
+              "manifest.json",
+              null,
+              extensionApi.extension.rootURI
+            );
+            extensionApi.chromeHandle = aomStartup.registerChrome(manifestURI, chromeData);
+          }
+
+          for (let res of resourceData) {
+            // [ "resource", "shortname" , "path" ]
+            let uri = Services.io.newURI(
+              res[2],
+              null,
+              extensionApi.extension.rootURI
+            );
+            resProto.setSubstitutionWithFlags(
+              res[1],
+              uri,
+              resProto.ALLOW_CONTENT_ACCESS
+            );
+          }
+
+          extensionApi.chromeData = chromeData;
+          extensionApi.resourceData = resourceData;
         }
 
       }
     };
   }
 
-  close() {
+  onShutdown(isAppShutdown) {
+    if (isAppShutdown) {
+      return; // the application gets unloaded anyway
+    }
+
     ExtensionSupport.unregisterWindowListener(
       "windowListener_" + this.uniqueRandomID,
     );
@@ -121,6 +185,25 @@ var LegacyCSS = class extends ExtensionCommon.ExtensionAPI {
       for (let element of elements) {
         element.remove();
       }
+    }
+
+    // Flush all caches
+    Services.obs.notifyObservers(null, "startupcache-invalidate");
+    this.registeredWindows = {};
+
+    if (this.resourceData) {
+      const resProto = Cc[
+        "@mozilla.org/network/protocol;1?name=resource"
+      ].getService(Ci.nsISubstitutingProtocolHandler);
+      for (let res of this.resourceData) {
+        // [ "resource", "shortname" , "path" ]
+        resProto.setSubstitution(res[1], null);
+      }
+    }
+
+    if (this.chromeHandle) {
+      this.chromeHandle.destruct();
+      this.chromeHandle = null;
     }
   }
 };
