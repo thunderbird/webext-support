69f8fb465f7a3a897499a055bf27ad8419301b4e
John Bieling <john.bieling@gmx.de>
2023-07-27 14:45:22 +0200

    Updated to work with TB102 and TB115

--- auxiliary-apis/FileSystem/implementation.js
+++ auxiliary-apis/FileSystem/implementation.js
@@ -2,6 +2,9 @@
  * This file is provided by the addon-developer-support repository at
  * https://github.com/thundernest/addon-developer-support
  *
+ * Version 1.2
+ * - adjusted to properly work with TB115 and TB102
+ *
  * Version 1.1
  * - adjusted to Thunderbird Supernova (Services is now in globalThis)
  *
@@ -19,19 +22,12 @@
 var { ExtensionCommon } = ChromeUtils.import(
   "resource://gre/modules/ExtensionCommon.jsm"
 );
-var { ExtensionSupport } = ChromeUtils.import(
-  "resource:///modules/ExtensionSupport.jsm"
-);
 var { ExtensionUtils } = ChromeUtils.import(
   "resource://gre/modules/ExtensionUtils.jsm"
 );
 var { ExtensionError } = ExtensionUtils;
 
-// Needed in TB78 to get the profile folder. In TB90 we can use
-// PathUtils to get the profile folder.
-var { FileUtils } = ChromeUtils.import("resource://gre/modules/FileUtils.jsm");
-var Services = globalThis.Services || 
-  ChromeUtils.import("resource://gre/modules/Services.jsm").Services;
+Cu.importGlobalProperties(["IOUtils", "PathUtils"]);
 
 var FileSystem = class extends ExtensionCommon.ExtensionAPI {
   getAPI(context) {
@@ -39,18 +35,16 @@ var FileSystem = class extends ExtensionCommon.ExtensionAPI {
       FileSystem: {
 
         readFile(filename) {
-          let win = Services.wm.getMostRecentWindow("mail:3pane", true);
-          let fileObj = FileUtils.getFile("ProfD", ["FileSystemAPI", context.extension.id, filename]);
-          if (fileObj.exists()) {
-            return win.IOUtils.readUTF8(fileObj.path)
+          let filePath = PathUtils.join(PathUtils.profileDir, "FileSystemAPI", context.extension.id, filename);
+          if (IOUtils.exists(filePath)) {
+            return IOUtils.readUTF8(filePath)
           }
-          throw new ExtensionError(`File at "${fileObj.path}" does not exist`)
+          throw new ExtensionError(`File at "${filePath}" does not exist`)
         },
 
         writeFile(filename, data) {
-          let win = Services.wm.getMostRecentWindow("mail:3pane", true);
-          let fileObj = FileUtils.getFile("ProfD", ["FileSystemAPI", context.extension.id, filename]);
-          return win.IOUtils.writeUTF8(fileObj.path, data)
+          let filePath = PathUtils.join(PathUtils.profileDir, "FileSystemAPI", context.extension.id, filename);
+          return IOUtils.writeUTF8(filePath, data)
         },
 
       }
