b9ca47a2aec313ded34097adc77b2ac779324610
John Bieling <john.bieling@gmx.de>
2024-09-17 14:05:42 +0200

    Update preferences.mjs

--- modules/preferences/preferences.mjs
+++ modules/preferences/preferences.mjs
@@ -18,10 +18,10 @@ export async function getPref(name) {
 // Ignore default values and return null if preference not set.
 export async function getUserPref(name) {
     let rv = await browser.storage.local.get({ [name]: null });
-    return rv.name;
+    return rv[name];
 }
 
 // Set preference.
 export async function setPref(name, value) {
-    await browser.storage.local.get({ [name]: value });
+    await browser.storage.local.set({ [name]: value });
 }
