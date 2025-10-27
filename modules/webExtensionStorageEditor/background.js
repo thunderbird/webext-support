3d960ee193d5445e497095957b500357c259f0ae
John Bieling <john.bieling@gmx.de>
2025-10-27 12:47:27 +0100

    update webExtensionStorageEditor

--- modules/webExtensionStorageEditor/background.js
+++
@@ -1,4 +0,0 @@
-import * as webExtensionStorageEditor from './modules/webExtensionStorageEditor.mjs'
-browser.browserAction.onClicked.addListener(async () => {
-    webExtensionStorageEditor.open({type:"popup"});
-});
